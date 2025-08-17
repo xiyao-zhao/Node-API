a0f666a0c95cb290545d8a844a6bd887df6cebfd
Xiyao Zhao <xiyao.zhao28@gmail.com>
2025-08-17 18:12:59 -0400

    connect to mongodb, mongoose, and use colors

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 // const logger = require("./middleware/logger");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -22,9 +27,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.bold
 	)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	// Closer server & exit process
+	server.close(() => process.exit(1));
+});
